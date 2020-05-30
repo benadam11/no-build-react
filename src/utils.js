92ab6a4f3098cb6e10efd522c93ee6b08b1156f3
Ben Adam <benjamin.mark.adam@gmail.com>
2020-05-30 15:27:55 -0700

    Move files around

--- src/utils.js
+++
@@ -1,6 +0,0 @@
-import { React, ReactDOM } from "https://cdn.pika.dev/es-react";
-import htm from "https://cdn.pika.dev/htm";
-
-export const html = htm.bind(React.createElement);
-export const { render } = ReactDOM;
-export const { useState } = React;
